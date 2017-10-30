238270d3d3d99fbdca0971037065f92a995a5e67
Mohit Bansal <aryashi4@gmail.com>
2017-10-30 06:51:30 +0000

    js file

---
+++ main.js
@@ -0,0 +1,48 @@
+
+function editdata()
+{
+    var m=document.getElementById("search_data");
+    var input = m.getElementById("myInput");\
+    var filter = input.value.toUpperCase();
+    var table = document.getElementById("tableid");
+    var tr = table.getElementsByTagName("tr");
+    for (i = 0; i < tr.length; i++) 
+    {
+      var td = tr[i].getElementsByTagName("td")[1];
+      if (td)
+       {
+          if (td.innerHTML.toUpperCase().indexOf(filter) > -1)
+            {
+              tr[i].style.display = "";
+           }
+            else 
+            {
+              tr[i].style.display = "none";
+            }
+       }
+    }   
+}
+function hide()
+{
+        $('td:nth-child(4)').hide();
+        $('td:nth-child(3)').hide();
+}
+ function inputdata() {
+        const row=document.createElement('tr');
+        let col1=document.createElement('td');
+        let col2=document.createElement('td');
+        let col3=document.createElement('td');
+        let col4=document.createElement('td');
+             row.appendChild(col1);
+             row.appendChild(col2);
+             row.appendChild(col3);
+             row.appendChild(col4);
+             col1.innerHTML=document.getElementById('roll').value;
+             col2.innerHTML=document.getElementById('name').value;
+             col3.innerHTML=document.getElementById('year').value;
+             col4.innerHTML=document.getElementById('stream').value;
+        const table=document.getElementById('tableid');
+             table.appendChild(row);
+        
+
+ }
