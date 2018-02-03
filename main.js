384fddab0c44ad2d2fff0b13f202565b9de4d3bd
Mohit Bansal <Mohit Bansal>
2018-02-03 12:09:30 +0530

    update

--- main.js
+++ main.js
@@ -6,7 +6,7 @@ function input_data() {
         const roll = document.getElementById("rollno").value;
         const year = document.getElementById("year").value;
         const stream = document.getElementById("stream").value;
-
+        
         if(name == null || /[^a-zA-Z]/.test(name))
                 alert("Enter correct Name");
         else if( roll == null || /[^0-9]/.test(roll))
@@ -17,7 +17,7 @@ function input_data() {
                 alert("Enter correct Stream");
         else if(name!= null && name.trim() !== '' && roll!= null && roll.trim() !== '' && year!= null && year.trim() !== '' && stream!= null && stream.trim() !== '')
         {
-            rows += `<tr><td>${roll}</td><td>${name}</td><td>${year}</td><td>${stream}</td></tr>`;
+            rows += `<tr><td><input class="c" type="checkbox" /><td>${roll}</td><td>${name}</td><td>${year}</td><td>${stream}</td></tr>`;
             $(rows).appendTo("#tableid");
         }
         else 
@@ -32,12 +32,17 @@ function input_data() {
         document.getElementById("stream").value = "";
     }
     function delete_data() {
-        const i = prompt("enter the row number which you want to delete ");
-        let t = document.getElementsByTagName('tr');
-        if(i<1 || i>t.length+1)
-        alert('No such row no. exists');
-        else
-        document.getElementById("tableid").deleteRow(i-1);
+        let t=document.getElementById("tableid");
+        let n=t.getElementsByClassName("c");
+        for(let i=0;i<=n.length;i++)
+        {
+            if(n[i].checked)
+            {
+                tableid.deleteRow(i);
+                i--;
+                n.length--;
+            }
+        }
     }
 
     function edit_data(){
